251549bcf3327550459fefd140c3992f67c340d4
Nao Hashizume <nhashizume.ca@gmail.com>
2020-02-27 10:29:46 -0800

    updated event receiver service

--- EventReceiverService/app.py
+++ EventReceiverService/app.py
@@ -1,24 +1,50 @@
 import connexion
 from connexion import NoContent
 import requests
-
+from pykafka import KafkaClient
+import yaml
+import datetime
+import json
 STORE_SERVICE_SELL_REQUEST_URL = "http://localhost:8090/sell_request"
 STORE_SERVICE_BUY_REQUEST_URL = "http://localhost:8090/buy_request"
-HEADERS = {"content-type":"application/json"}
+HEADERS = {"content-type" : "application/json"}
+
+with open('app_conf.yaml', 'r') as f:
+    app_config = yaml.safe_load(f.read())
+
+
+client = KafkaClient(hosts='%s:%i' % (app_config['kafka']['server'], app_config['kafka']['port']))
+topic = client.topics['%s' %(app_config['kafka']['topic'])]
 
 
 def add_sell_request(sell_request):
     """ Receives a request for selling an item """
-    response = requests.post(STORE_SERVICE_SELL_REQUEST_URL, json=sell_request, headers=HEADERS)
+    # response = requests.post(STORE_SERVICE_SELL_REQUEST_URL, json=sell_request, headers=HEADERS)
+    producer = topic.get_sync_producer()
+    msg = {"type": "sr",
+           "datetime":
+               datetime.datetime.now().strftime(
+                   "%Y-%m-%dT%H:%M:%S"),
+           "payload": sell_request}
+    msg_str = json.dumps(msg)
+    producer.produce(msg_str.encode('utf-8'))
 
-    return NoContent, response.status_code
+    return NoContent, 200
 
 
 def add_buy_request(buy_request):
     """ Receives a request for buying an item """
-    response = requests.post(STORE_SERVICE_BUY_REQUEST_URL, json=buy_request, headers=HEADERS)
+    # response = requests.post(STORE_SERVICE_BUY_REQUEST_URL, json=buy_request, headers=HEADERS)
+    producer = topic.get_sync_producer()
+    msg = {"type": "br",
+           "datetime":
+               datetime.datetime.now().strftime(
+                   "%Y-%m-%dT%H:%M:%S"),
+           "payload": buy_request}
+    msg_str = json.dumps(msg)
+    producer.produce(msg_str.encode('utf-8'))
 
-    return NoContent, response.status_code
+    return NoContent, 200
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
